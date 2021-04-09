bd97e78cd87949f75ff8cf740cfbc557993d371e
ToTem-oss <ro.sym@yandex.ru>
2021-04-09 23:53:37 +0300

    lf

--- scrits.js
+++ scrits.js
@@ -7,21 +7,18 @@ if(key==true){
 Swal.fire('Я это сделал, а ' + names + ' тебе от меня совет: “ Поступай правильно — поступай в ВятГУ!”')
 }
 else{
-Swal.fire({
-title: 'Введите имя',
-input: 'text', inputAttributes: {
-autocapitalize: 'off'
-},
-showCancelButton: true,
-cancelButtonText: 'Отмена',
-confirmButtonText: 'Ввести',
-allowOutsideClick: () => !Swal.isLoading()
-}).then((result) => {
-if (result.isConfirmed) {
-Swal.fire('Я это сделал, а ' + result.value + ' тебе от меня совет: “ Поступай правильно — поступай в ВятГУ!”')
+
+{
+Swal.fire('Макет был реализован Сусловым Романом Владимировичем, вариант 4')
 names=result.value;
 }
-})
 key=true;
 }
 }
+
+type="text/javascript">$(window).on('load', function () {
+    var $preloader = $('#p_prldr'),
+        $svg_anm   = $preloader.find('.svg_anm');
+    $svg_anm.fadeOut();
+    $preloader.delay(500).fadeOut('slow');
+});
